81169a93efcedfeeb1b9bef22950dcbb184669fa
acphan <acphan1993@yahoo.com>
2018-01-02 18:39:43 -0800

    changes
    changes

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -9,7 +9,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
@@ -18,12 +19,3 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
-
-
-class List(models.Model):
-    name = models.CharField(max_length=100)
-
-
-class Word(models.Model):
-    list = models.ForeignKey(List, on_delete=models.CASCADE)
-    text = models.CharField(max_length=30)
